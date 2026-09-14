d4dee4749c1134f3fe26f96a942623809d2e02a7
kesselbuettner-dot <kesselbuettner@googlemail.com>
2026-09-14 21:39:09 +0200

    Update AppUserRepository.java

--- src/main/java/de/bierverein/api/AppUserRepository.java
+++ src/main/java/de/bierverein/api/AppUserRepository.java
@@ -1,7 +1,22 @@
 package de.bierverein.api;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
-public interface AppUserRepository extends JpaRepository<AppUser,Long>{
- Optional<AppUser> findByUsername(String username);
- boolean existsByUsername(String username);
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findByUsername(String username);
+
+    @Query("""
+        select u
+        from AppUser u
+        left join fetch u.member
+        where u.username = :username
+    """)
+    Optional<AppUser> findByUsernameWithMember(@Param("username") String username);
+
+    boolean existsByUsername(String username);
 }
