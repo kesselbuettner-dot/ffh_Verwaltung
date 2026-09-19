9168a14fcc4e3c6e6a4d8f5eb35c8406544c9f25
kesselbuettner-dot <kesselbuettner@googlemail.com>
2026-09-19 10:03:07 +0200

    feat: add Geratewart device inspection backend

---
+++ src/main/java/de/bierverein/api/DeviceInspectionRepository.java
@@ -0,0 +1,6 @@
+package de.bierverein.api;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
+public interface DeviceInspectionRepository extends JpaRepository<DeviceInspection,Long>{
+    List<DeviceInspection> findByDeviceIdOrderByInspectionDateDesc(Long deviceId);
+}
