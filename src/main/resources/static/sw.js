f300715891e5c95d3f842588db580747142b478a
kesselbuettner-dot <kesselbuettner@googlemail.com>
2026-09-21 22:44:34 +0200

    Refresh service worker cache to deliver new OCR and Wehrleitung UI after server update

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "ffh-verwaltung-wehrleiter-v36";
+const CACHE_NAME = "ffh-verwaltung-wehrleiter-ocr-v37";
 const APP_SHELL = [
   "/",
   "/manifest.json",
   "/menu-designer.js?v=577e741-v2",
   "/menu-designer.css?v=577e741-v2",
-  "/wehrleiter.js?v=1",
-  "/wehrleiter.css?v=1",
+  "/wehrleiter.js?v=2",
+  "/wehrleiter.css?v=2",
   "/ui-theme.css?v=1",
   "/icons/fw-cockpit-brand.svg",
   "/icons/fw-cockpit-icon-192.png",
