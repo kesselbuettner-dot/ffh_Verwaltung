f03c14938cd0b4754740b6af931cd0c22e7f0c56
kesselbuettner-dot <kesselbuettner@googlemail.com>
2026-09-21 21:53:21 +0200

    Keep birth date, joining date and administrator-defined member fields in separate protected profile tables

---
+++ src/main/java/de/bierverein/api/MemberCustomFieldRepository.java
@@ -0,0 +1,3 @@
+package de.bierverein.api;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface MemberCustomFieldRepository extends JpaRepository<MemberCustomField,Long>{}
