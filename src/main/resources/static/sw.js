9e54610a0a7eb4c717928e962e6aff13ef6de7cf
kesselbuettner-dot <kesselbuettner@googlemail.com>
2026-09-22 11:59:44 +0200

    Invalidate PWA cache and load new three-column qualification table and print stylesheet

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "ffh-verwaltung-wehrleiter-documents-v38";
+const CACHE_NAME = "ffh-verwaltung-wehrleiter-three-columns-print-v39";
 const APP_SHELL = [
   "/",
   "/manifest.json",
   "/menu-designer.js?v=577e741-v2",
   "/menu-designer.css?v=577e741-v2",
-  "/wehrleiter.js?v=3",
-  "/wehrleiter.css?v=3",
+  "/wehrleiter.js?v=4",
+  "/wehrleiter.css?v=4",
   "/ui-theme.css?v=2",
   "/icons/fw-cockpit-brand.svg",
   "/icons/fw-cockpit-icon-192.png",
