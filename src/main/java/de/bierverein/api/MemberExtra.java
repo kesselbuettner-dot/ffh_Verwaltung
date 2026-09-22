fa8ad5b989bd3b2d5f1c77804570bb833f5ca224
kesselbuettner-dot <kesselbuettner@googlemail.com>
2026-09-22 07:13:22 +0200

    Add optional abbreviated qualification label and private member avatar fields without changing prior data

--- src/main/java/de/bierverein/api/MemberExtra.java
+++ src/main/java/de/bierverein/api/MemberExtra.java
@@ -6,6 +6,8 @@ public class MemberExtra {
  @Id public Long memberId;
  public LocalDate birthDate;
  public LocalDate joinedOn;
+ @Column(name="avatar_mime",length=20) public String avatarMime;
+ @Column(name="avatar_data",columnDefinition="bytea") public byte[] avatarData;
  @Column(columnDefinition="TEXT") public String customJson="{}";
  protected MemberExtra(){}
  public MemberExtra(Long memberId){this.memberId=memberId;}
