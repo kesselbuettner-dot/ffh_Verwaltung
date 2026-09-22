fa62ff2ab1c87458896a70c339a52472af617ad4
kesselbuettner-dot <kesselbuettner@googlemail.com>
2026-09-22 17:22:43 +0200

    Unified inspections: schema and account deletion DeviceInspectionRepository.java

--- src/main/java/de/bierverein/api/DeviceInspectionRepository.java
+++ src/main/java/de/bierverein/api/DeviceInspectionRepository.java
@@ -3,4 +3,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 public interface DeviceInspectionRepository extends JpaRepository<DeviceInspection,Long>{
     List<DeviceInspection> findByDeviceIdOrderByInspectionDateDesc(Long deviceId);
+    List<DeviceInspection> findBySessionReportIdOrderByIdAsc(Long reportId);
 }
