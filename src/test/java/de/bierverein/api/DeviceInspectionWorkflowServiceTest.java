29a62f82db64cd9764f4495861e09f7d62246a71
kesselbuettner-dot <kesselbuettner@googlemail.com>
2026-09-22 17:32:16 +0200

    Use explicit AWT imports in workflow regression tests

--- src/test/java/de/bierverein/api/DeviceInspectionWorkflowServiceTest.java
+++ src/test/java/de/bierverein/api/DeviceInspectionWorkflowServiceTest.java
@@ -1,6 +1,8 @@
 package de.bierverein.api;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.BasicStroke;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.time.*;
